1652b290dda3df7f8b1ebd122b004a038e155e0a
ushibutatory <ushibutatory@gmail.com>
2020-06-22 15:49:01 +0900

    update: dotnet version

---
+++ src/FizzBuzzSolution/FizzBuzz.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FizzBuzz.App
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var start = 1;
+            var end = 100;
+
+            foreach (var result in Counter.Count(start, end))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
