2ce18c7fed854bf1674fc1f70188c384007643e1
ushibutatory <ushibutatory@gmail.com>
2020-09-15 15:28:41 +0900

    comment

--- src/FizzBuzzSolution/NabeAtsu.Core/States/IState.cs
+++ src/FizzBuzzSolution/NabeAtsu.Core/States/IState.cs
@@ -3,14 +3,35 @@ using System.Numerics;
 
 namespace NabeAtsu.Core.States
 {
+    /// <summary>
+    /// 状態インタフェース
+    /// </summary>
     public interface IState
     {
+        /// <summary>
+        /// 有効かどうか
+        /// </summary>
+        /// <remarks>
+        /// 現状常にTrueなので、無意味な機能です。
+        /// </remarks>
         public bool Enabled { get; }
 
+        /// <summary>
+        /// 内包する状態
+        /// </summary>
         public IEnumerable<IState> SubStates { get; }
 
+        /// <summary>
+        /// 条件に当てはまるかどうか
+        /// </summary>
+        /// <param name="value">数値</param>
         public bool IsApplied(BigInteger value);
 
+        /// <summary>
+        /// 数値を文字列に変換します。
+        /// </summary>
+        /// <param name="value">数値</param>
+        /// <returns>変換結果</returns>
         public Result Convert(BigInteger value);
     }
 }
